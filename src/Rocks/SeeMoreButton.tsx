33e4125372072599242e99f361b0eb754d4a66f0
BenLee3 <bendesigns76@gmail.com>
2019-03-21 18:45:09 +0900

    Added Rocks folder

---
+++ src/Rocks/SeeMoreButton.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { Button } from "react-bootstrap";
+
+export default function SeeMoreButton(props: any) {
+  return <Button variant='info' onClick={this.props.clicked} />;
+}
