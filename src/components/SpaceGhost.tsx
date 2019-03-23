b98845763e73147fa8d2deaeac611172455961aa
BenLee3 <bendesigns76@gmail.com>
2019-03-24 02:04:08 +0900

    Added a SG comp to master.

---
+++ src/components/SpaceGhost.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { Button } from "react-bootstrap";
+
+export default function SeeMoreButton(props: any) {
+  return (
+  <Button variant='info' onClick={this.props.clicked} />
+  <Button variant='info' onClick={this.props.clicked} />
+  <Button variant='info' onClick={this.props.clicked} />
+}
